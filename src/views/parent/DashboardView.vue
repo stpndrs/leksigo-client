bf169da0f5220cd7bc9aa13f254250efeb976924
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2025-11-16 19:37:12 +0700

    feat: slicing dashboard

--- src/views/parent/DashboardView.vue
+++ src/views/parent/DashboardView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { authStore } from '@/stores/AuthStore';
 import { onMounted, ref } from 'vue';
+import child from '@/assets/images/child.png'
+import CopyIcon from '@/components/shape/CopyIcon.vue';
 
 const fullName = ref()
 onMounted(() => {
@@ -13,5 +15,210 @@ onMounted(() => {
         <div class="page-header">
             <h1 class="page-title">Selamat Datang, {{ fullName }}</h1>
         </div>
+        <div class="page-body">
+            <div class="grid-container">
+                <div class="item">
+                    <div>
+                        <img :src="child" alt="Child" />
+                        <div class="identity">
+                            <span class="level level-1">1</span>
+                            <span class="name">Budi Pratama</span>
+                        </div>
+                        <span class="teacher">| Nama Guru</span>
+                        <span class="code">
+                            <CopyIcon /> <span>#<strong>code</strong></span>
+                        </span>
+                    </div>
+                    <div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div>
+                        <img :src="child" alt="Child" />
+                        <div class="identity">
+                            <span class="level level-1">1</span>
+                            <span class="name">Budi Pratama</span>
+                        </div>
+                        <span class="teacher">| Nama Guru</span>
+                        <span class="code">
+                            <CopyIcon /> <span>#<strong>code</strong></span>
+                        </span>
+                    </div>
+                    <div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div>
+                        <img :src="child" alt="Child" />
+                        <div class="identity">
+                            <span class="level level-1">1</span>
+                            <span class="name">Budi Pratama</span>
+                        </div>
+                        <span class="teacher">| Nama Guru</span>
+                        <span class="code">
+                            <CopyIcon /> <span>#<strong>code</strong></span>
+                        </span>
+                    </div>
+                    <div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div>
+                        <img :src="child" alt="Child" />
+                        <div class="identity">
+                            <span class="level level-1">1</span>
+                            <span class="name">Budi Pratama</span>
+                        </div>
+                        <span class="teacher">| Nama Guru</span>
+                        <span class="code">
+                            <CopyIcon /> <span>#<strong>code</strong></span>
+                        </span>
+                    </div>
+                    <div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                        <div class="exercise">
+                            <h3>Latihan 1, baru permulaan hehe</h3>
+                            <p>Total Skor : 90</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.level {
+    padding: 10px 15px;
+    border-radius: 5px;
+
+    &.level-1 {
+        background-color: var(--Secondary-500);
+        color: var(--White);
+    }
+
+    &.level-2 {
+        background-color: var(--Ternary-300);
+        color: var(--White);
+    }
+
+    &.level-3 {
+        color: var(--White);
+        background-color: var(--Primary-500);
+    }
+}
+
+.grid-container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 30px;
+
+    .item {
+        display: grid;
+        gap: 15px;
+        grid-template-columns: repeat(2, 1fr);
+        background-color: var(--White);
+        padding: 20px;
+        border-radius: 10px;
+
+
+        div:nth-child(1) {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: start;
+            gap: 10px;
+
+            img {
+                width: 100%;
+                display: block;
+            }
+
+            .identity {
+                display: flex;
+                justify-content: start;
+                align-items: center;
+                gap: 15px;
+            }
+
+            .name,
+            .score {
+                color: var(--Secondary-900);
+                font-size: 25px;
+                font-weight: bold;
+            }
+
+            .teacher,
+            .code {
+                font-size: 20px;
+                color: var(--Secondary-900);
+            }
+
+            .code {
+                display: flex;
+                justify-content: start;
+                align-items: center;
+                gap: 10px;
+            }
+        }
+
+        div:nth-child(2) {
+            .exercise {
+                background-color: var(--Secondary-500);
+                color: var(--White);
+                padding: 15px;
+                border-radius: 10px;
+                border: 3px solid var(--Secondary-900);
+                margin-bottom: 10px;
+
+                h3 {
+                    font-weight: bold;
+                }
+            }
+        }
+    }
+}
+</style>
--- src/views/teacher/DashboardView.vue
+++ src/views/teacher/DashboardView.vue
@@ -13,5 +13,178 @@ onMounted(() => {
         <div class="page-header">
             <h1 class="page-title">Selamat Datang, {{ fullName }}</h1>
         </div>
+
+        <div class="page-body">
+            <div class="grid-container">
+                <div class="card score">
+                    <div class="card-header">
+                        <h4>Perolehan Skor</h4>
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            <h1>80</h1>
+                            <p>Skor Tertinggi</p>
+                        </div>
+                        <div>
+                            <h1>65</h1>
+                            <p>Skor Terendah</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card child">
+                    <div class="card-header">
+                        <h4>Perolehan Skor Anak Didik</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="item">
+                            <span class="identity">
+                                <span class="level level-2">2</span>
+                                <span class="name">Budi Pratama</span>
+                            </span>
+                            <span class="score">67</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card total">
+                    <div class="card-header">
+                        <h4>Jumlah Anak Didik</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="item">
+                            <span class="level level-1">Level 1</span>
+                            <h3>2</h3>
+                            <p>Anak</p>
+                        </div>
+                        <div class="item">
+                            <span class="level level-2">Level 2</span>
+                            <h3>2</h3>
+                            <p>Anak</p>
+                        </div>
+                        <div class="item">
+                            <span class="level level-3">Level 3</span>
+                            <h3>2</h3>
+                            <p>Anak</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+
+<style lang="scss" scoped>
+.grid-container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    gap: 8px;
+
+    .card {
+        padding: 30px;
+        background-color: var(--White);
+        border-radius: 15px;
+    }
+
+    .card-header {
+        font-size: 20px;
+        color: var(--Secondary-900);
+        margin-bottom: 15px;
+    }
+
+    .card.score {
+        .card-body {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            text-align: center;
+            color: var(--Secondary-900);
+
+            h1 {
+                font-size: 80px;
+            }
+        }
+    }
+
+    .level {
+        padding: 10px 15px;
+        border-radius: 5px;
+
+        &.level-1 {
+            background-color: var(--Secondary-500);
+            color: var(--White);
+        }
+
+        &.level-2 {
+            background-color: var(--Ternary-300);
+            color: var(--White);
+        }
+
+        &.level-3 {
+            color: var(--White);
+            background-color: var(--Primary-500);
+        }
+    }
+
+    .card.child {
+        grid-row: span 2 / span 2;
+
+        .card-body {
+            .item {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 15px;
+
+                .identity {
+                    display: flex;
+                    justify-content: start;
+                    align-items: center;
+                    gap: 15px;
+                }
+
+                .name,
+                .score {
+                    color: var(--Secondary-900);
+                    font-size: 25px;
+                    font-weight: bold;
+                }
+            }
+        }
+    }
+
+    .card.total {
+        grid-row-start: 2;
+
+        .card-body {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .item {
+                color: var(--Secondary-900);
+                text-align: center;
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
+
+                h3 {
+                    font-size: 45px;
+                    font-weight: bold;
+                }
+            }
+        }
+    }
+
+    /* Target Tablet (Medium) */
+    @media (max-width: 1024px) {
+        grid-template-columns: 1fr;
+    }
+
+    /* Target Ponsel (Kecil) */
+    @media (max-width: 768px) {
+        .card {
+            padding: 20px;
+        }
+    }
+}
+</style>
